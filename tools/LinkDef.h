552d209ce61bd6a3d0ac55c290265580bbc0d48e
José I. Crespo-Anadón <jcrespo@nevis.columbia.edu>
2018-01-22 17:46:19 -0500

    Remove tools directory. Decoder and plotter moved to own repository at NevisSBND/DecoderTools.

--- tools/LinkDef.h
+++
@@ -1,4 +0,0 @@
-#ifdef __MAKECINT__
-#pragma link C++ class HeaderInfo+;
-#pragma link C++ class vector< vector<uint16_t> >+;
-#endif
--- tools/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Generating dictionary of decoder.hh and HeaderInfo.hh\n"
-rootcint -f decoder_dict.cc -c decoder.hh HeaderInfo.hh LinkDef.h
-echo "Compiling decoder.cc\n"
-g++ decoder_dict.cc decoder.cc -Wall -o decoder.exe `root-config --cflags  --glibs`
-echo "Compiling plotter.cc\n"
-g++ decoder_dict.cc plotter.cc -Wall -o plotter.exe `root-config --cflags  --glibs`
